39bf00c9f5b1063684d0b06bacfe2c6b5af3fea3
bagus kromo sampurno <baguskromosampurno@gmail.com>
2020-04-07 22:44:00 +0700

    tugas 2 js
    tugas kami kumpulkan untuk Niomic

---
+++ tugas_2_js.js
@@ -0,0 +1,21 @@
+var number = 1;
+console.log(number)
+
+var string = "test";
+console.log(string)
+
+var a = 0; //boolean
+if (a > -1) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+var b = null; //null
+console.log(b)
+
+var c; //undefined
+console.log(c)
+
+var d = [1, 2, 3, 4];
+console.log(d)
